ebdf651227f579bc11d3ffdfd63a1b859210f099
rcardin <riccardo.cardin@gmail.com>
2022-10-28 15:25:55 +0200

    Added a skeleton for the Spring Boot project

---
+++ src/main/java/in/rcard/swe/imdb/SweImdbApplication.java
@@ -0,0 +1,13 @@
+package in.rcard.swe.imdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SweImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SweImdbApplication.class, args);
+	}
+
+}
